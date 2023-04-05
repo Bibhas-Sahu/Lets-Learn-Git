78f9cdf368e042564ea1c103b00357b026c09ef9
Bibhas Sahu <bibhassahu8984@gmail.com>
2023-04-05 18:42:37 +0530

    added msg in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String[] args){
         int a=10;
         int b=30;
-        System.out.println(a+b);
+        System.out.println("Sum = "+a+b);
     }
 }
